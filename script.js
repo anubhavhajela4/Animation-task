65f931f3da4c6241df1be3565f5421143142310b
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-10-08 20:03:34 +0530

    Added the game logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const container = document.getElementById("container");
 const bucket =document.getElementById("bucket");
+let scoreDisplay=document.getElementById("score");
+let heartDisplay=document.getElementById("life");
+let points=0;
+let hearts=3;
+let gameActive=true;
 document.addEventListener('keydown',(event) => {
     let currentPos = bucket.offsetLeft;
     if(event.key==='ArrowLeft') {
@@ -17,9 +22,44 @@ document.addEventListener('keydown',(event) => {
         bucket.style.left=`${currentPos}px`;
     }
 });
-const drop = document.createElement("div");
-drop.setAttribute("class","rain");
-let pos = `${Math.random()*1500}px`;
-container.appendChild(drop);
-drop.style.left=pos;
+
+function fallingRain () {
+    if(!gameActive) {
+        return;
+    }
+    const drop = document.createElement("div");
+    drop.setAttribute("class","rain");
+    let pos = Math.random()*1500;
+    drop.style.left=`${pos}px`;
+    drop.style.top=`${0}px`;
+    container.appendChild(drop);
+    let fall = 0;
+    const verPos = setInterval(()=> {
+        if(fall<container.offsetHeight-40) {
+            fall+=5;
+            drop.style.top =`${fall}px`;
+            const dropRect = drop.getBoundingClientRect();
+            const bucketRect = bucket.getBoundingClientRect();
+            if(dropRect.bottom >=bucketRect.top && dropRect.top <= bucketRect.bottom && dropRect.right >= bucketRect.left && dropRect.left <= bucketRect.right) {
+                points++;
+                scoreDisplay.innerText =`Score : ${points}`;
+                clearInterval(verPos);
+                container.removeChild(drop);
+            }
+        }
+        else {
+            clearInterval(verPos);
+            container.removeChild(drop);
+            hearts--;
+            if(hearts<=0) {
+                gameActive=false;
+                alert("Game Over");
+            }
+            else {
+                heartDisplay.innerText=`Lives :${hearts}`;
+            }
+        }
+    },30);
+}
+setInterval(fallingRain,2000);
 
